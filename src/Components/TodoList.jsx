f5318250a69304aff730b3c387add05aa5bfe291
suruur <surrarweesh@gmail.com>
2024-07-26 20:46:12 -0400

    todo list

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+
+    const [todos, setTodos] = useState([])
+    const [headingInput, setHeadinginput] = useState('')
+    const [listInputs, setListinputs] = useState({})
+    
+    const handleAddTodo = () =>{
+        if(headingInput.trim() !== null){
+            setTodos([...todos, {
+                heading:headingInput,
+                lists: []
+            }])
+            setHeadinginput('')
+        }
+    }
   
 
   return (
@@ -13,13 +27,29 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value = {headingInput}
+            onChange={(e) => {setHeadinginput(e.target.value)}}
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
+        {todos.map( (todo, idx) => (
+            <div key={idx} className='todo-card'>
+               <div className="heading_todo">
+                <h3>{todo.heading}</h3>
+                <button 
+                className='delete-button-heading'
+                onClick={()=> handleDeleteTodo(idx)}
+                > Delete Heading</button>
+               </div>
+            </div>
+
+        )
+
         
+
+        )}
       </div>
     </>
   );
